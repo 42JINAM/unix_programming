ce6b75895246b63f5a9a1ac40aeff4ba84d9d3c2
42JINAM <jinam@student.42.fr>
2026-02-25 17:16:31 +0100

    process control

---
+++ 1.7.c
@@ -0,0 +1,35 @@
+#include "include/apue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/_types/_pid_t.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	char	buf[MAXLINE];
+	pid_t	pid;
+	int		status;
+
+	printf("%% ");
+	while (fgets(buf, MAXLINE, stdin) != NULL)
+	{
+		if (buf[strlen(buf) - 1] == '\n')
+			buf[strlen(buf) - 1] = 0;
+		pid = fork();
+		if (pid < 0)
+			err_sys("fork error");
+		else if (pid == 0)
+		{
+			execlp(buf, buf, (char *) 0);
+			err_ret("couldn't execute: %s", buf);
+			exit(127);
+		}
+		pid = waitpid(pid, &status, 0);
+		if (pid < 0)
+			err_sys("waitpid error");
+		printf("%% ");
+	}
+	exit(0);
+}
--- err_utils.c
+++ err_utils.c
@@ -35,3 +35,13 @@ void	err_sys(const char *fmt, ...)
 	va_end(ap);
 	exit(1);
 }
+
+void	err_ret(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
